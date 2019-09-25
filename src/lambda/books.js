fff4a94fc68aa0789520ba50beeb2db2c019ae6e
Daniel Lee <dan@dansadeveloper.com>
2019-09-25 17:50:56 -0400

    fixing netlify escaping backslashes in private key

--- src/lambda/books.js
+++ src/lambda/books.js
@@ -5,7 +5,7 @@ const scopes = [
 ];
 
 const clientEmail = process.env.client_email;
-const privateKey = process.env.private_key;
+const privateKey = process.env.private_key.replace(/\\\\n/g, '\\n');
 const folderId = process.env.folder_id;
 const downloadString = process.env.download_string;
 
